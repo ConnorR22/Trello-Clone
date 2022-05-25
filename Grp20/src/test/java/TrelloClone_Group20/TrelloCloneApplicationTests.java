125141754ead3f71b8624f63e3d8233a304dad18
Connor Russell <55206366+J3thro209@users.noreply.github.com>
2022-05-25 19:58:22 -0300

    Added SpringBoot Project Files

---
+++ Grp20/src/test/java/TrelloClone_Group20/TrelloCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package TrelloClone_Group20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
