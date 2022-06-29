5e17ce38d366d12c424676fbbab26d042b2931db
Lynda Ofume <Ly863136@dal.ca>
2022-06-28 21:31:30 -0300

    updates to pages in trello

--- trello-clone/src/pages/Register.js
+++ trello-clone/src/pages/Register.js
@@ -1,7 +1,19 @@
-import Registerform from "../stylings/Registrationform";
-import React, {useRef} from 'react';
+import Registerform from "../stylings/RegisterForm";
+
 
 function Registration(){
+
+  function userHandler(user){
+    fetch('db.cs.dal.ca/users.json' , {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {'Content-Type' : 'application/json'}
+    })
+   
+
+
+  }
+
   return (
   <div>
     <h1> Register Page</h1>
---
+++ trello-clone/src/stylings/RegisterForm.js
@@ -0,0 +1,32 @@
+
+import React, { useRef } from 'react';
+
+function Registerform(){
+
+   const fullNameRef = useRef();
+   const emailRef = useRef();
+   const passwordRef = useRef();
+
+   function submitHandler(event){
+      event.preventDefault();
+      
+      const fullName = fullNameRef.current.value;
+      const email = emailRef.current.value;
+      const password = passwordRef.current.value;
+      const user = {fullName, email, password};
+
+      console.log(user);
+   }
+
+   return (
+       <form onSubmit={submitHandler}>
+        <input type="text" required placeholder="Full Name" ref={fullNameRef} />
+        <input type="email" required placeholder="Email Address" ref={emailRef} />
+        <input type="password" required placeholder="Password" ref={passwordRef} />
+        <button> Register Now </button>
+       </form>
+    );
+  
+  }
+  export default Registerform;
+  
