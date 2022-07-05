d8e734d102fbcfa2b5680e3b36f4b0c292266ed7
Lynda Ofume <Ly863136@dal.ca>
2022-07-05 15:38:01 -0300

    updates to reset password function

--- trello-clone/src/components/forms/ResetPasswordForm.js
+++ trello-clone/src/components/forms/ResetPasswordForm.js
@@ -16,7 +16,9 @@ function ResetPassForm(props){
 
    return (
        <form onSubmit={ResetPassHandler}>
-        <input type="password" required placeholder="Reset Password" ref={passwordRef} />
+        <input pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{0,8}"
+         title="Must contain at least one number and one uppercase and lowercase letter, and at least 8" 
+         type="password" required placeholder="Reset Password" ref={passwordRef} />
         <br></br>
         <button> Reset Password </button>
        </form>
